b6d7d2f11ad4718ee8bae9280bbc10a071b47d22
HR <@ayibaruelt.github.io>
2024-02-16 21:00:29 +0600

    final commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Drawable_dashboard_Rabiya"
+include(":app")
+ 
